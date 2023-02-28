08a87afc0614eb33eea311cd358212960d800ee2
jezwev <43579360+jezwev@users.noreply.github.com>
2023-02-28 08:54:23 -0500

    Breakfast Menu

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,9 @@
 import streamlit
 
 streamlit.title('My parents healthly diner')
+
+streamlit.header('Breakfast Menu')
+
+streamlit.text('Omega 3 Blueberry Oatmeal')
+streamlit.text('Kale, Spinach and Rocket SMoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
