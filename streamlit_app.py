1d8bab82bdfe34e72a075b8c0801e22b4c25e27c
jezwev <43579360+jezwev@users.noreply.github.com>
2023-03-08 08:12:03 -0500

    Normalize semi-structured JSON

--- streamlit_app.py
+++ streamlit_app.py
@@ -22,4 +22,8 @@ streamlit.dataframe(fruits_to_show)
 streamlit.header("Fruityvice Fruit Advice!")
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response.json())
+
+# Normalize semi-structured JSON data into a flat table.
+fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+# DIsplay in streamlit table strucutre
+streamlit.dataframe(fruityvice_normalized)
