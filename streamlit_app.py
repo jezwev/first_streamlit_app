251274d74eb51ad4ccd6fc4db219aa887c27baa4
jezwev <43579360+jezwev@users.noreply.github.com>
2023-03-14 09:15:38 -0400

    insert to snwflake

--- streamlit_app.py
+++ streamlit_app.py
@@ -63,5 +63,10 @@ if streamlit.button('Get fruit load list'):
     streamlit.dataframe(my_data_row)
 
 #allow end user to add fruit to list
+def insert_row(new_fruit):
+     with my_cnx.cursor() as my_cur:
+        my_cur.execute("insert into fruit_load_list values('fromstreamlit')")
+        return "Thanks for adding " + new_fruit
+
 add_my_fruit = streamlit.text_input('What fruit would you like to add?','')
-streamlit.write('The user entered ', add_my_fruit)
+streamlit.write('The user entered ', insert_row(add_my_fruit))
